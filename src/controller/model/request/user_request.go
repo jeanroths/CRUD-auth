4ac6a4fb4a9be91dcb3f13a9a1f491be3fa1f0ee
jeanroths <jean.machado@sou.inteli.edu.br>
2024-03-12 11:02:10 -0300

    crud teste8

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,7 +1,7 @@
 package request 
 
 type UserRequest struct {
-	Name string `json "name" binding:"required, min=4, max=60"`
+	Name string `json "name" binding:"required,min=4,max=60"`
 	Email string `json "email" binding:""`
 	Password string `json "password" binding:""`
 }
