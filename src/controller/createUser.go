d1afc70a796bf6f7a6058dc0933b105851814da9
jeanroths <jean.machado@sou.inteli.edu.br>
2024-03-12 10:54:03 -0300

    crud teste8

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,8 +3,19 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jeanroths/CRUD-auth/src/controller/model/request"
+	"github.com/jeanroths/CRUD-auth/src/configuration/rest_err"
+	"fmt"
 )
 func CreateUser(c *gin.Context){
 	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil	{
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("Invalid fields, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 	// code to create user
 }
--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,7 +1,7 @@
 package request 
 
 type UserRequest struct {
-	Name string 
-	Email string 
-	Password string 
+	Name string `json "name" binding:"required, min=4, max=60"`
+	Email string `json "email" binding:""`
+	Password string `json "password" binding:""`
 }
