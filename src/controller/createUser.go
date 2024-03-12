c8f09b29efcace21cd3efab70da500b7198927d9
jeanroths <jean.machado@sou.inteli.edu.br>
2024-03-12 09:22:38 -0300

    crud teste5

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
-
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jeanroths/CRUD-auth/tree/main/src/configuration/rest_err"
+)
 func CreateUser(c *gin.Context){
+	err := rest_err.NewBadRequestError("Rota chamada de maneira errada")
+	c.JSON(err.Code, err)
 	// code to create user
 }
--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -6,9 +6,9 @@ import (
 )
 
 func InitRoutes(r *gin.RouterGroup){
-	r.GET("/getUserById/:userId", controller.findUserByID)
-	r.GET("/getUserByEmail/:userEmail", controller.findUserByEmail)
-	r.POST("/createUser", controller.createUser)
-	r.PUT("/updateUser/:userId", controller.updateUser)
-	r.DELETE("/deleteUser/:userId", controller.deleteUser)
+	r.GET("/getUserById/:userId", controller.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
 }
